a11c1da4a0272b99f372e48bc3b9f8be4d5d9732
Jonathan Tan <jhow.tan@gmail.com>
2014-12-16 14:49:29 +0800

    Express generate

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../.config.json');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/login', function(req, res) {
+  res.redirect('https://ivle.nus.edu.sg/api/login/?apikey=' + config.apikey  + '&url=http://localhost:3000/ivle');
+});
+
+router.get('/ivle', function(req, res) {
+  console.log(req.query.token);
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
