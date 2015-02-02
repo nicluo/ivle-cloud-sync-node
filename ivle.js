145327b9fb6864b8d2b39109e79302b180a981ce
Jonathan Tan <jhow.tan@gmail.com>
2015-02-02 22:25:13 +0800

    Improve camelCase

--- ivle.js
+++ ivle.js
@@ -8,7 +8,7 @@ var profile = function profile(token, callback) {
   var url = 'https://ivle.nus.edu.sg/api/Lapi.svc/Profile_View?APIKey=' + config.apikey + '&AuthToken=' + token;
 
   _request(url, function(err, body){
-    var profile = _camelCaseResponseBody(body);
+    var profile = _camelCaseObject(body);
 
     callback(err, profile);
   }, singular=true);
@@ -17,7 +17,11 @@ var profile = function profile(token, callback) {
 var modules = function modules(token, callback){
   var url = 'https://ivle.nus.edu.sg/api/Lapi.svc/Modules_Student?APIKey=' + config.apikey + '&AuthToken=' + token + '&Duration=0&IncludeAllInfo=true';
 
-  _request(url, callback);
+  _request(url, function(err, body){
+    var mods = _camelCaseObjectOrArray(body);
+
+    callback(err, mods);
+  });
 };
 
 var _request = function _request(url, callback, singular){
@@ -33,10 +37,34 @@ var _request = function _request(url, callback, singular){
 };
 
 // camelCase keys returned from IVLE Api
-var _camelCaseResponseBody = function _camelCaseResponseBody(body){
-  return _(body).pairs().map(function(pair){
-    return [_.camelCase(pair[0]), pair[1]];
+var _camelCaseObjectOrArray = function _camelCaseObjectOrArray(body) {
+  if (_.isArray(body))
+    return _camelCaseArray(body);
+  else if (_.isPlainObject(body))
+    return _camelCaseObject(body);
+  else
+    return body;
+};
+
+var _camelCaseObject = function _camelCaseObject(obj){
+  return _(obj).pairs().map(function(pair){
+    var value = pair[1];
+    if (_.isArray(value))
+      value = _camelCaseArray(value);
+    else if (_.isPlainObject(value))
+      value = _camelCaseObject(value);
+    return [_.camelCase(pair[0]), value];
   }).object().value();
+};
+
+var _camelCaseArray = function _camelCaseArray(arr) {
+  return _(arr).map(function(value) {
+    if (_.isArray(value))
+      return _camelCaseArray(value);
+    else if (_.isPlainObject(value))
+      return _camelCaseObject(value);
+    else return value;
+  }).value();
 };
 
 module.exports = {
