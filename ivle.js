cc34060bd4dc9eabe355e539d6a5ff5e9b80f0c0
Jonathan Tan <jhow.tan@gmail.com>
2015-02-02 22:40:51 +0800

    Clean _camelCaseDeep

--- ivle.js
+++ ivle.js
@@ -7,37 +7,31 @@ var login_url = 'https://ivle.nus.edu.sg/api/login/?apikey=' + config.apikey  + 
 var profile = function profile(token, callback) {
   var url = 'https://ivle.nus.edu.sg/api/Lapi.svc/Profile_View?APIKey=' + config.apikey + '&AuthToken=' + token;
 
-  _request(url, function(err, body){
-    var profile = _camelCaseObject(body);
-
-    callback(err, profile);
-  }, singular=true);
+  _request(url, callback, singular=true);
 };
 
 var modules = function modules(token, callback){
   var url = 'https://ivle.nus.edu.sg/api/Lapi.svc/Modules_Student?APIKey=' + config.apikey + '&AuthToken=' + token + '&Duration=0&IncludeAllInfo=true';
 
-  _request(url, function(err, body){
-    var mods = _camelCaseObjectOrArray(body);
-
-    callback(err, mods);
-  });
+  _request(url, callback);
 };
 
 var _request = function _request(url, callback, singular){
   request(url, function(error, response, body) {
     var jsonBody = JSON.parse(body);
 
+    var results = _camelCaseDeep(jsonBody.Results);
+
     if(singular){
-      return callback(error, jsonBody.Results[0]);
+      return callback(error, results[0]);
     }
 
-    callback(error, jsonBody.Results);
+    callback(error, results);
   });
 };
 
 // camelCase keys returned from IVLE Api
-var _camelCaseObjectOrArray = function _camelCaseObjectOrArray(body) {
+var _camelCaseDeep = function _camelCaseDeep(body) {
   if (_.isArray(body))
     return _camelCaseArray(body);
   else if (_.isPlainObject(body))
@@ -49,21 +43,13 @@ var _camelCaseObjectOrArray = function _camelCaseObjectOrArray(body) {
 var _camelCaseObject = function _camelCaseObject(obj){
   return _(obj).pairs().map(function(pair){
     var value = pair[1];
-    if (_.isArray(value))
-      value = _camelCaseArray(value);
-    else if (_.isPlainObject(value))
-      value = _camelCaseObject(value);
-    return [_.camelCase(pair[0]), value];
+    return [_.camelCase(pair[0]), _camelCaseDeep(value)];
   }).object().value();
 };
 
 var _camelCaseArray = function _camelCaseArray(arr) {
   return _(arr).map(function(value) {
-    if (_.isArray(value))
-      return _camelCaseArray(value);
-    else if (_.isPlainObject(value))
-      return _camelCaseObject(value);
-    else return value;
+    return _camelCaseDeep(value);
   }).value();
 };
 
