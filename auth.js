9b0bbd4252cd632cdd9e080a6f28c26274dcaf27
Jonathan Tan <jhow.tan@gmail.com>
2015-02-14 00:03:45 +0800

    auth inherits passport, reveal middleware (render)

--- auth.js
+++ auth.js
@@ -2,24 +2,33 @@ var passport = require('passport');
 var NusIvleStrategy = require('./passport-nus-ivle');
 var User = require('./models/user');
 
-passport.use(new NusIvleStrategy(
-  function(token, profile, done) {
-    User.findOne({ 'userId': profile.userId }, function (err, user) {
-      if (err) { return done(err); }
-
-      if (!user) { return done(null, false); }
-
-      if (user) { return done(null, user); }
-
-      var newUser = new User(profile);
-
-      newUser.token = token;
-
-      newUser.save(function (err, savedUser) {
+passport.use(new NusIvleStrategy({
+    passReqToCallback: true
+  }, function(req, token, profile, done) {
+    // Verify Callback
+    if (!req.user) {
+      // Not logged in. Authenticate based on profile.userId.
+      User.findOne({ 'userId': profile.userId }, function (err, user) {
         if (err) { return done(err); }
-        return done(null, user);
+
+        if (!user) { return done(null, false); }
+
+        if (user) { return done(null, user); }
+
+        // Registration if new user
+        var newUser = new User(profile);
+
+        newUser.token = token;
+
+        newUser.save(function (err, savedUser) {
+          if (err) { return done(err); }
+          return done(null, user);
+        });
       });
-    });
+    } else {
+      // Logged in. Preserve login state.
+      return done(null, req.user);
+    }
   }
 ));
 
@@ -31,4 +40,12 @@ passport.deserializeUser(function(user, done){
   done(null, user);
 });
 
-module.exports = passport;
+// Auth inherits passport
+var auth = Object.create(passport);
+
+auth.populateLocals = function(req, res, next){
+    res.locals.user = req.user;
+    next();
+};
+
+module.exports = auth;
